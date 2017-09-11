68ac43e83d18d41f2c288cff950560631286765a
Teagen Kiel <teagen-kiel@uiowa.edu>
2017-09-11 11:48:37 -0500

    Created files for S1 Computus

--- class_demos/ch08/fig08_01_02/src/Time1Test.java
+++ class_demos/ch08/fig08_01_02/src/Time1Test.java
@@ -1,7 +1,8 @@
 // Fig. 8.2: Time1Test.java
 // Time1 object used in an app.
 
-public class Time1Test {
+public class
+Time1Test {
     public static void main(String[] args) {
         // create and initialize a Time1 object
         Time1 time = new Time1(); // invokes Time1 constructor
