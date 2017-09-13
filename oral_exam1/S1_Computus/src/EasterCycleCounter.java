6456b7842b6a168a0e1da88c8cfc91e6998ea5d2
Teagen Kiel <teagen-kiel@uiowa.edu>
2017-09-12 23:45:12 -0500

    Fixing driver class to accurately test object classes

---
+++ oral_exam1/S1_Computus/src/EasterCycleCounter.java
@@ -0,0 +1,64 @@
+/**
+ * Created by teagenkiel on 9/12/17.
+ */
+public class EasterCycleCounter {
+
+
+    private final int monthIndexMax = 2;
+    private final int dayIndexMax = 31;
+    private final int monthIndexOffset = 3;
+    private final int dayIndexOffset = 1;
+
+    private int[][] dateCounterArray = new int[monthIndexMax][dayIndexMax];
+    private EasterCalculator calculatorForCycle;
+
+    public EasterCycleCounter(EasterCalculator calculatorForCycle){
+
+        this.calculatorForCycle = calculatorForCycle;
+
+        this.countEasters();
+
+    }
+
+    private void countEasters() {
+
+        final int easterCycleMax = 5700000;
+        final int easterCycleMin = 0;
+
+        System.out.println("Calculating...");
+
+        for (int yearCounter = easterCycleMin; yearCounter < easterCycleMax; yearCounter++) {
+
+            this.calculatorForCycle.setYear(yearCounter);
+            this.calculatorForCycle.computus();
+            dateCounterArray[(this.calculatorForCycle.getMonthAsInteger()) - monthIndexOffset]
+                    [(this.calculatorForCycle.getDay()) - dayIndexOffset]++;
+
+        }
+    }
+
+    public void printEasterCount(){
+
+        final int indexMin = 0;
+        final int emptyArrayValue = 0;
+
+        for (int monthIndexCounter = indexMin; monthIndexCounter < monthIndexMax; monthIndexCounter++) {
+
+            for (int dayIndexCounter = indexMin; dayIndexCounter < dayIndexMax; dayIndexCounter++){
+
+                if(dateCounterArray[monthIndexCounter][dayIndexCounter] != emptyArrayValue){
+
+                    System.out.println(
+                            Calendar.getMonthName(monthIndexCounter + monthIndexOffset) + " " +
+                                    (dayIndexCounter + dayIndexOffset) + ": " +
+                                    dateCounterArray[monthIndexCounter][dayIndexCounter]
+
+                    );
+
+                }
+
+            }
+        }
+    }
+
+}
